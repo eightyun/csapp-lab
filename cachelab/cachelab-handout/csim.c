71ba3338527c65b2995e2e8f19760bb867b05a1a
eightyun <879950144@qq.com>
2023-06-30 21:03:37 +0800

    finish cache lab

--- cachelab/cachelab-handout/csim.c
+++ cachelab/cachelab-handout/csim.c
@@ -1,7 +1,153 @@
 #include "cachelab.h"
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
 
-int main()
+typedef struct
 {
-    printSummary(0, 0, 0);
+    int valid_bits;         // 有效位
+    unsigned  tag;          // 标记位
+    int stamp;              // 时间戳 用来实行lru算法
+}cache_line;
+
+char* filepath = NULL;
+int s , E , b , S ;
+int hit = 0 , miss = 0 , eviction = 0;
+cache_line** cache = NULL;
+
+void init()
+{
+    cache = (cache_line**)malloc(sizeof(cache_line * ) * S);  //开一个大空间大指针
+
+    for(int i = 0 ; i < S ; i++)
+        *(cache+i) = (cache_line*)malloc(sizeof(cache_line) * E);  // 再在大空间里面开一个个小指针小空间
+
+    for(int i = 0 ; i < S ; i++)
+    {
+        for(int j = 0 ; j < E ; j++)
+        {
+            cache[i][j].valid_bits = 0;      // set all valid_bits is zero
+            cache[i][j].tag = 0xffffffff;           //no address
+            cache[i][j].stamp = 0;            //time is 0;
+        }
+    }
+}
+void update(unsigned address)
+{
+    unsigned address_s =(address >> b) & ((0xffffffff) >> (32 - s));         // 设置组索引
+    unsigned address_t = address>>(s + b);                                 //     设置标记位
+
+    for(int i = 0 ; i < E ; i++)
+    {
+        if((*(cache+address_s)+i)->tag == address_t){
+            cache[address_s][i].stamp = 0;              //被使用了 所以置零
+            hit++;
+            return;
+        }
+    }
+    for(int i = 0 ; i < E ; i++)
+    {
+        if(cache[address_s][i].valid_bits == 0)
+        {
+            cache[address_s][i].tag = address_t;
+            cache[address_s][i].valid_bits = 1 ;
+            cache[address_s][i].stamp = 0;              // 载入数据
+            miss++;
+            return;
+        }
+    }
+
+    int max_stamp=0;
+    int max_i;
+    for(int i = 0 ; i < E ; i++)
+    {
+        if(cache[address_s][i].stamp >max_stamp)
+        {
+            max_stamp = cache[address_s][i].stamp;
+            max_i = i;
+        }
+    }
+
+    eviction++;
+    miss++;
+    cache[address_s][max_i].tag = address_t;                 //  62到剩下的为替换过程
+    cache[address_s][max_i].stamp = 0;
+}
+void time()
+{
+    for(int i = 0 ; i < S ;i++)
+    {
+        for(int j = 0 ; j < E ; j++)
+        {
+            if(cache[i][j].valid_bits == 1)
+                cache[i][j].stamp++;
+        }
+    }
+}
+int main(int argc,char *argv[])
+{
+    int opt;
+    while((opt = getopt(argc,argv,"s:E:b:t:")) !=-1)   //   处理输入参数
+    {
+        switch(opt)
+        {
+            case 's':
+                s=atoi(optarg);         // 字符串转数字函数
+                break;
+
+            case 'E':
+                E=atoi(optarg);
+                break;
+
+            case 'b':
+                b=atoi(optarg);
+                break;
+
+            case 't':
+                filepath = optarg;
+                break;
+        }
+    }
+
+    S = 1 << s;     // 设置组数
+    init();
+
+    FILE* tracefile=fopen(filepath,"r");
+    if(tracefile == NULL)                   //读取追踪文件
+    {
+        printf("Open file wrong");
+        exit(-1);
+    }
+
+    char operation;
+    unsigned address;
+    int size;
+    while(fscanf(tracefile," %c %x,%d",&operation,&address,&size)>0)
+    {
+        switch(operation)
+        {
+            case 'L':
+                update(address);
+                break;
+
+            case 'M':
+                update(address);
+
+            case 'S':
+                update(address);
+                break;
+        }
+
+        time();
+    }
+
+    for(int i = 0 ; i < S ;i++)
+        free(*(cache+i));
+    free(cache);
+    fclose(tracefile);	                // 关闭文件
+
+    printSummary(hit,miss,eviction);
     return 0;
 }
--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -12,6 +12,117 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
+char transpose1[] = "Transpose 1";
+void transpose11(int M, int N, int A[N][M], int B[M][N])
+{
+    int i , j;
+    int i1;
+    int val1,val2,val3,val4,val5,val6,val7,val8;
+    for(i = 0 ; i < 32 ; i += 8)
+        for(j = 0 ; j < 32 ; j += 8)
+            for(i1 = i ; i1 < i + 8 ; i1++)
+            {
+                val1 = A[i1][j + 0];
+                val2 = A[i1][j + 1];
+                val3 = A[i1][j + 2];
+                val4 = A[i1][j + 3];
+                val5 = A[i1][j + 4];
+                val6 = A[i1][j + 5];
+                val7 = A[i1][j + 6];
+                val8 = A[i1][j + 7];
+                B[j + 0][i1] = val1;
+                B[j + 1][i1] = val2;
+                B[j + 2][i1] = val3;
+                B[j + 3][i1] = val4;
+                B[j + 4][i1] = val5;
+                B[j + 5][i1] = val6;
+                B[j + 6][i1] = val7;
+                B[j + 7][i1] = val8;
+            }
+}
+
+char transpose2[] = "Transpose 2" ;
+void transpose22(int M , int N , int A[N][M] , int B[M][N])
+{
+    int i, j, x, y;
+    int x1, x2, x3, x4, x5, x6, x7, x8;
+    for (i = 0; i < N; i += 8)
+        for (j = 0; j < M; j += 8)
+        {
+            for (x = i; x < i + 4; ++x)
+            {
+                x1 = A[x][j]; x2 = A[x][j+1]; x3 = A[x][j+2]; x4 = A[x][j+3];
+                x5 = A[x][j+4]; x6 = A[x][j+5]; x7 = A[x][j+6]; x8 = A[x][j+7];
+
+                B[j][x] = x1; B[j+1][x] = x2; B[j+2][x] = x3; B[j+3][x] = x4;
+                B[j][x+4] = x5; B[j+1][x+4] = x6; B[j+2][x+4] = x7; B[j+3][x+4] = x8;
+            }
+            for (y = j; y < j + 4; ++y)
+            {
+                x1 = A[i+4][y]; x2 = A[i+5][y]; x3 = A[i+6][y]; x4 = A[i+7][y];
+                x5 = B[y][i+4]; x6 = B[y][i+5]; x7 = B[y][i+6]; x8 = B[y][i+7];
+
+                B[y][i+4] = x1; B[y][i+5] = x2; B[y][i+6] = x3; B[y][i+7] = x4;
+                B[y+4][i] = x5; B[y+4][i+1] = x6; B[y+4][i+2] = x7; B[y+4][i+3] = x8;
+            }
+            for (x = i + 4; x < i + 8; ++x)
+            {
+                x1 = A[x][j+4]; x2 = A[x][j+5]; x3 = A[x][j+6]; x4 = A[x][j+7];
+                B[j+4][x] = x1; B[j+5][x] = x2; B[j+6][x] = x3; B[j+7][x] = x4;
+            }
+        }
+}
+
+char transpose3[] = "Transpose 3" ;
+void transpose33(int M , int N , int A[N][M] , int B[M][N])
+{
+    int i, j, v1, v2, v3, v4, v5, v6, v7, v8;
+    int n = N / 8 * 8;
+    int m = M / 8 * 8;
+    for (j = 0; j < m; j += 8)
+        for (i = 0; i < n; ++i)
+        {
+            v1 = A[i][j];
+            v2 = A[i][j+1];
+            v3 = A[i][j+2];
+            v4 = A[i][j+3];
+            v5 = A[i][j+4];
+            v6 = A[i][j+5];
+            v7 = A[i][j+6];
+            v8 = A[i][j+7];
+
+            B[j][i] = v1;
+            B[j+1][i] = v2;
+            B[j+2][i] = v3;
+            B[j+3][i] = v4;
+            B[j+4][i] = v5;
+            B[j+5][i] = v6;
+            B[j+6][i] = v7;
+            B[j+7][i] = v8;
+        }
+
+    for (i = n; i < N; ++i)
+        for (j = m; j < M; ++j)
+        {
+            v1 = A[i][j];
+            B[j][i] = v1;
+        }
+
+    for (i = 0; i < N; ++i)
+        for (j = m; j < M; ++j)
+        {
+            v1 = A[i][j];
+            B[j][i] = v1;
+        }
+
+    for (i = n; i < N; ++i)
+        for (j = 0; j < M; ++j)
+        {
+            v1 = A[i][j];
+            B[j][i] = v1;
+        }
+}
+
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -22,6 +133,14 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M == 32)
+        return transpose11(32 , 32 , A , B);
+
+    else if(M == 64)
+        return transpose22(64 , 64 , A , B);
+
+    else if(M == 61)
+        return transpose33(M , N , A , B);
 }
 
 /* 
@@ -37,13 +156,12 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0 ; i < N ; i++)
+        for (j = 0 ; j < M ; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
 }
 
 /*
@@ -59,8 +177,7 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -72,13 +189,13 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
                 return 0;
-            }
         }
-    }
+
     return 1;
 }
 
