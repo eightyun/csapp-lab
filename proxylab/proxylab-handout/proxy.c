4ff14883c88348122457f06efd09478092f0d5e2
eightyun <879950144@qq.com>
2023-08-31 00:00:18 +0800

    proxy 第二部分代码

--- proxylab/proxylab-handout/proxy.c
+++ proxylab/proxylab-handout/proxy.c
@@ -5,6 +5,8 @@
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+#define NTHREADS 4
+#define SBUFSIZE 16
 
 struct Uri
 {
@@ -13,20 +15,36 @@ struct Uri
     char path[MAXLINE]; // 路径
 };
 
-struct sbuf_t
+typedef struct
 {
-    
-};
+    int * buf ;  // buffer array
+    int n ;     // maximum number of slots
+    int front ; // buf[(front + 1) % n] is fitst item
+    int rear ;  // buf[rear % n] is last item
+    sem_t mutex ;  // protects accesses to buf
+    sem_t slots ;  // count s available slots
+    sem_t items ; // counts available items
+}sbuf_t;
+
+sbuf_t sbuf ;
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
+// 客户端与服务端相关签名
 void doit(int fd);
 void parse_uri(const char *uri, struct Uri *uri_data);
 void build_header(char *header, const char *host, const char *path);
 void send_request(int serverfd, char *header);
 void receive_response(int connfd, int serverfd);
 void sigpipe_handler(int signum);
+
+//多线程相关签名
+void sbuf_init (sbuf_t* sp , int n) ;
+void sbuf_insert (sbuf_t * sp , int item) ;
+int sbuf_remove (sbuf_t * sp) ;
+void * thread (void * vargp) ;
+
 
 void doit(int connfd)
 {
@@ -134,12 +152,58 @@ void receive_response(int connfd, int serverfd)
     }
 }
 
+void sbuf_init(sbuf_t * sp , int n)
+{
+    sp -> buf = Calloc(n , sizeof(int)) ; // 分配内存
+    sp -> n = n ; // buffer holds max of n items
+    sp -> front = sp ->rear = 0 ; // empty buffer iff front == rear
+    Sem_init(&sp -> mutex , 0 , 1) ; // binary semaphore for locking
+    Sem_init(&sp -> slots , 0 , n) ; // initially buf has n empty slots
+    Sem_init(&sp -> items , 0 , 0) ; // initially buf has zero data items
+}
+
+void sbuf_insert(sbuf_t * sp , int item)
+{
+    P(&sp -> slots) ; // wait for available slot
+    P(&sp -> mutex) ; // lock the buffer
+    sp -> buf[(++sp -> rear) % (sp -> n)] = item ; //sp->rear 是下一个可插入位置的索引，sp->n 表示缓冲区的大小。通过对 sp->rear 进行自增并取模运算，可以循环利用缓冲区。
+    V(&sp -> mutex) ; // unlock the buffer
+    V(&sp -> items) ; // announce available item
+}
+
+int sbuf_remove(sbuf_t * sp)
+{
+    int item;
+    P(&sp -> items);                          /* Wait for available item */
+    P(&sp -> mutex);                          /* Lock the buffer */
+    item = sp -> buf[(++sp -> front)%(sp -> n)];  /* Remove the item */
+    V(&sp -> mutex);                          /* Unlock the buffer */
+    V(&sp -> slots);                          /* Announce available slot */
+    return item;
+}
+
+void * thread (void * vargp) // 线程执行函数
+{
+    Pthread_detach(pthread_self());
+
+    while(1)
+    {
+        //从缓冲区中读出描述符
+        int connfd = sbuf_remove(&sbuf);
+        //转发
+        doit(connfd);
+        //关闭客户端的连接描述符
+        Close(connfd) ;
+    }
+}
+
 int main(int argc, char **argv)
 {
     int listenfd, connfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
+    pthread_t tid ;
 
     if (argc != 2)
         unix_error("proxy usage: ./proxy <port number>");
@@ -147,15 +211,17 @@ int main(int argc, char **argv)
     Signal(SIGPIPE, sigpipe_handler);
     listenfd = Open_listenfd(argv[1]);
 
+    sbuf_init(&sbuf , SBUFSIZE) ; // 初始化线程数据
+    for (int i = 0 ; i < NTHREADS ; i++) // create worker threads
+        Pthread_create(&tid , NULL , thread , NULL) ;
+
     while (1)
     {
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        sbuf_insert(&sbuf , connfd) ; // 向缓冲区写入描述符
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
         printf("Accepted connection from (%s %s).\n", hostname, port);
-
-        doit(connfd);
-        Close(connfd);
     }
 
     Close(listenfd); // 关闭监听描述符
